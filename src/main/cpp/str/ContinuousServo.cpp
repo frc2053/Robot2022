75ec4410dc2f070c1f163d1688b9b3547d26ee67
Drew Williams <williams.r.drew@gmail.com>
2022-03-13 01:51:39 -0500

    changed hood to match servo

--- src/main/cpp/str/ContinuousServo.cpp
+++ src/main/cpp/str/ContinuousServo.cpp
@@ -1,7 +1,7 @@
 #include "str/ContinuousServo.h"
 
 ContinuousServo::ContinuousServo(int channel) : frc::Servo(channel) {
-    SetBounds(1.9, 1.89, 1.550, 1.11, 1.1);
-    SetPeriodMultiplier(frc::Servo::kPeriodMultiplier_4X);
+    SetBounds(1.800, 1.560, 1.545, 1.530, 1.300);
+    SetPeriodMultiplier(frc::Servo::kPeriodMultiplier_1X);
     SetSpeed(0.0);
 }
